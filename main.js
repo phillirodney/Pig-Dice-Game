45cafd408e9b3154510f2017d6101c9823b7ffa7
phillirodney <phillirodney@gmail.com>
2019-07-19 12:47:15 +0100

    Roll condiitons working, player 1, Values presented in UI

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+var accumPoints = 0;
+
 function player1Roll() {
   /**
    * Player 1 rolls 2 dice, generate random numbers
@@ -5,5 +7,43 @@ function player1Roll() {
    */
   dice1 = Math.ceil(Math.random() * 6);
   dice2 = Math.ceil(Math.random() * 6);
-  console.log(dice1, dice2);
+  // calcualte the sum of the rolled dice
+  rollSum = dice1 + dice2;
+  // show sum of rolled dice in UI
+  document.getElementById("p1-roll-sum").innerHTML = rollSum;
+  rollConditions();
 }
+
+function player2Roll() {}
+function rollConditions() {
+  /**
+   * if snake eyes
+   * lose turn
+   * rollSum  = 0
+   * all accumulated points = 0
+   * show values in UI
+   */
+  if (dice1 == 1 && dice2 == 1) {
+    rollSum = 0;
+    accumPoints = 0;
+    document.getElementById("p1-acc-pts").innerHTML = accumPoints;
+    console.log("Ooops, snake eyes ");
+  } else if (dice1 == 1 || dice2 == 1) {
+    /**
+     * if a 1 is rolled
+     * rollSum = 0
+     * accumulated points unchaged
+     * show values in UI
+     */
+    rollSum = 0;
+    accumPoints += rollSum;
+    document.getElementById("p1-acc-pts").innerHTML = accumPoints;
+    console.log("Ooops, you rolled a 1 ");
+  } else {
+    accumPoints += rollSum;
+    document.getElementById("p1-acc-pts").innerHTML = accumPoints;
+    console.log(dice1, dice2, rollSum);
+  }
+}
+function hold() {}
+function swapPlayer() {}
